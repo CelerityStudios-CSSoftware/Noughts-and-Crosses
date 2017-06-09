ab35165e00e270d692a9e096638c7cf1289447af
Christopher Steel <christopher.g.steel@gmail.com>
2017-06-09 14:50:13 +0200

    Initial commit. Generates a 3x3 grid and that's it

---
+++ Assets/GridGenerator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridGenerator : MonoBehaviour {
+    public Transform EmptyGridCell;
+    public uint width;
+    public uint height;
+
+	// Use this for initialization
+	void Start () {
+        for (uint x = 0; x < width; x++) {
+            for (uint y = 0; y < height; y++) {
+                Instantiate(EmptyGridCell, new Vector3(x, y, transform.position.z), Quaternion.identity, transform);
+            }
+        }
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
